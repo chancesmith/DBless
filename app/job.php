ea5d185971b902d3b1887aa3877d79e7f722075f
Chance Smith <chancesmithb@gmail.com>
2017-03-30 22:25:38 -0500

    adds single job view with URL variable

--- app/job.php
+++ app/job.php
@@ -1,6 +1,7 @@
 <?php 
 $fileJSON = file_get_contents("jobs.json");
 $jobs = json_decode( $fileJSON, true );
+$jobFound = false;
 ?>
 
 
@@ -8,7 +9,7 @@ $jobs = json_decode( $fileJSON, true );
 <head>
 	<meta charset="utf-8">
 
-	<title>Show Jobs | DBless</title>
+	<title>Single Job Listing | DBless</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!-- fonts -->
@@ -25,19 +26,25 @@ $jobs = json_decode( $fileJSON, true );
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
-				<h1>List of Available Jobs:</h1>
-				<a href="./#/">edit jobs</a>
+				<a href="./jobs.php" class="btn btn-default">view all jobs</a>
+				<a href="./#/" class="btn btn-default">edit all jobs</a>
 			</div>
 		<?php if ($jobs != '' || isset($jobs)): ?>
 			<?php foreach($jobs as $job) { //foreach element in job list ?>
-				<div class="col-md-8 col-md-offset-2">
-					<h2><?php echo $job['title']; ?></h2>
-					<p>Responsibilities: <?php echo $job['responsibilities']; ?></p>
-					<p>Description: <?php echo $job['description']; ?></p>
-					<p><a href="/job.php?job=<?php echo $job['slug']; ?>" class="btn btn-default">view this job</a></p>
-					<hr/>
-				</div>
+				<?php if (isset($_GET["job"]) && $_GET["job"] == $job['slug']) { ?>
+					<?php $jobFound = true; ?>
+					<div class="col-md-8 col-md-offset-2">
+						<h1><?php echo $job['title']; ?></h1>
+						<p>Responsibilities: <?php echo $job['responsibilities']; ?></p>
+						<p>Description: <?php echo $job['description']; ?></p>
+					</div>
+				<?php } ?>
 			<?php } ?>
+			<?php if (!$jobFound): ?>
+				<div class="col-md-8 col-md-offset-2">
+					<h2>Sorry, We can't fin the job you are looking for. <a href="./jobs.php" class="btn btn-default">Go view all jobs</a></h2>
+				</div>
+			<?php endif ?>
 		<?php else: ?>
 			<div class="col-md-8 col-md-offset-2">
 				<h2>Sorry, no career opportunities are avilable at this time. Come back later to see future udpates.</h2>
