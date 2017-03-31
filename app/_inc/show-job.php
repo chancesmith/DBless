da465979103f0397127ec0566dc0d43141a62de1
Chance Smith <chancesmithb@gmail.com>
2017-03-31 11:05:13 -0500

    fixes the jobs.php if no single job found error message

--- app/_inc/show-job.php
+++ app/_inc/show-job.php
@@ -1,6 +1,6 @@
 <?php foreach($jobs as $job) { //foreach element in job list ?>
 	<?php if (isset($_GET["job"]) && $_GET["job"] == $job['slug']) { ?>
-		<?php $jobFound = true; ?>
+		<?php $singleJobFound = true; ?>
 		<h1><?php echo $job['title']; ?></h1>
 		<p>Responsibilities: <?php echo $job['responsibilities']; ?></p>
 		<p>Description: <?php echo $job['description']; ?></p>
